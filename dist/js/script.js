c8f6f754ac49e6b6f99d667d12f43a5ae6d1e4ea
KIssShot-Shinobu <rifaldi.zk@gmail.com>
2022-11-13 19:27:02 +0700

    add hidden menu nav js

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,11 +3,16 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if(window.pageYOffset > fixedNav){
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -21,4 +26,11 @@ const navmenu = document.querySelector('#nav-menu');
 humberger.addEventListener('click',  function () {
     humberger.classList.toggle('humberger-active');
     navmenu.classList.toggle('hidden');
+});
+
+window.addEventListener('click', function (e) {
+    if (e.target != humberger && e.target != navmenu){
+     humberger.classList.remove('humberger-active');
+     navmenu.classList.add('hidden');   
+    }
 });
